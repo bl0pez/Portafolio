e304127abd1c7b37fa34a3523e867a4deb2d2c59
bryan lopez <blopez4434@gmail.com>
2023-12-07 15:12:05 -0300

    update seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 
+export const metadata: Metadata = {
+  title: 'Portafolio - Inicio',
+  description: 'Portafolio de Bryan López Sánchez',
+  openGraph: {
+    title: 'Portafolio - Inicio',
+    description: 'Portafolio de Bryan López Sánchez',
+    type: 'website',
+    locale: 'es_ES',
+    url: 'https://blopez.cl/',
+    images: [
+      {
+        url: 'https://bryanlopez.dev/hero.svg',
+        width: 500,
+        height: 500,
+        alt: 'Hero',
+      },
+    ],
+  },
+}
+
 export default function Home() {
+
   return (
     <main className="flex-auto flex container mx-auto items-center flex-wrap gap-2">
         <div className="text-center p-2 mx-auto">
